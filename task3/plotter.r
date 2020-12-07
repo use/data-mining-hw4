187019b567be464f876e559ce1310c57a1e6a616
Will Hall <willhall@outlook.com>
2020-12-06 22:16:47 -0800

    New version of R program which finds and separates the clusters itself

--- task3/plotter.r
+++ task3/plotter.r
@@ -1,12 +1,18 @@
-par(mfrow=c(2,3)) # create grid of graphs on a single image
-filenames <- list.files(pattern="cluster-.*.csv")
-for (file in filenames) {
-    data = read.csv(file)
-    ts.plot(
-      t(data), # transpose, or else the graphs will make no sense...
-      gpars=list(ylim=c(0,70)), # set range for the graph
-      col=rgb(red = 0, green = 0, blue = 1, alpha = 0.1)) # colors, with alpha
-      # if centroids are stored as the first item,
-      # the following can be used to highlight them on the graphs
-      lines(t(data)[,1], col='red')
+signals <- read.table("synthetic_control_data.txt")
+fit <- kmeans(signals, 6)
+signals <- cbind(signals, cluster=fit$cluster)
+
+par(mfrow=c(2,3))
+for (i in c(1:6)) {
+  ts.plot(
+    t(signals[signals["cluster"] == i,][1:60]),
+    gpars = list(ylim = c(0,70)),
+    col = rgb(red = 0, green = 0, blue = 1, alpha = 0.1)
+  )
+  write.table(
+    file = paste("cluster", i, ".csv", sep=""),
+    x = signals[signals["cluster"] == i,][1:60],
+    row.names = FALSE,
+    col.names = FALSE)
+  lines(fit$centers[i,], col='red')
 }
